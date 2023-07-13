08eeace7225a7f1e9ed42977b67a8c46497747c6
Neha Gupta <nehgupta@zynga.com>
2023-07-13 16:09:25 +0530

    worked

--- src/api.js
+++ src/api.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
+app.use(cors());
 
 router.get('/', (req, res) => {
 	res.json({
 		hello: 'hi!'
+	});
+});
+
+router.get('/test', (req, res) => {
+	res.json({
+		hello: 'hello!'
 	});
 });
 
